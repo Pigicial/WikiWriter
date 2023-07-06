40d195cb2ba6f59e4d125f648ffa79f001f082b0
Pigicial <72773248+Pigicial@users.noreply.github.com>
2023-07-05 19:29:52 -0700

    few tweaks to have less sonarlint issues

--- src/main/java/me/pigicial/wikiwriter/features/RecipeUtils.java
+++ src/main/java/me/pigicial/wikiwriter/features/RecipeUtils.java
@@ -9,8 +9,8 @@ public final class RecipeUtils {
     public static final int CRAFTING_TABLE_OR_RECIPE_REQUIRED_SLOT = 23;
     public static final int FORCE_RECIPE_RESULT_SLOT = 16;
 
-    public static final int[] CRAFTING_TABLE_INGREDIENT_SLOTS = new int[]{10, 11, 12, 19, 20, 21, 28, 29, 30};
-    public static final int[] FORGE_POSITION_TO_SLOT_MAP = new int[]{10, 11, 19, 20, 28, 29, 37, 38};
+    static final int[] CRAFTING_TABLE_INGREDIENT_SLOTS = new int[]{10, 11, 12, 19, 20, 21, 28, 29, 30};
+    static final int[] FORGE_POSITION_TO_SLOT_MAP = new int[]{10, 11, 19, 20, 28, 29, 37, 38};
 
     private RecipeUtils() {
 
