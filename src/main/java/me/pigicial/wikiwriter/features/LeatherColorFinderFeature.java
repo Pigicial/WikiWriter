866adb360ef66c6ea3ef7b1f2766939251948ca5
Pigicial <dmjkitchin@gmail.com>
2023-07-05 19:39:52 -0700

    less "new Color()" boilerplate

--- src/main/java/me/pigicial/wikiwriter/features/LeatherColorFinderFeature.java
+++ src/main/java/me/pigicial/wikiwriter/features/LeatherColorFinderFeature.java
@@ -31,27 +31,27 @@ public class LeatherColorFinderFeature {
     }
 
     public enum LeatherArmorColor {
-        GRAY(new Color(43, 47, 49)),
-        YELLOW(new Color(225, 207, 50)),
-        WHITE(new Color(255, 255, 255)),
-        GREEN(new Color(74, 101, 16)),
-        BLUE(new Color(58, 64, 152)),
-        LIME(new Color(111, 174, 27)),
-        RED(new Color(185, 41, 31)),
-        MAGENTA(new Color(172, 66, 163)),
-        CYAN(new Color(18, 130, 130)),
-        ORANGE(new Color(206, 107, 24)),
-        PURPLE(new Color(113, 41, 152)),
-        BLACK(new Color(24, 24, 27)),
-        PINK(new Color(202, 116, 142)),
-        LIGHT_BLUE(new Color(76, 176, 206)),
-        LIGHT_GRAY(new Color(131, 131, 126)),
-        DEFAULT(new Color(113, 71, 45));
+        GRAY(43, 47, 49),
+        YELLOW(225, 207, 50),
+        WHITE(255, 255, 255),
+        GREEN(74, 101, 16),
+        BLUE(58, 64, 152),
+        LIME(111, 174, 27),
+        RED(185, 41, 31),
+        MAGENTA(172, 66, 163),
+        CYAN(18, 130, 130),
+        ORANGE(206, 107, 24),
+        PURPLE(113, 41, 152),
+        BLACK(24, 24, 27),
+        PINK(202, 116, 142),
+        LIGHT_BLUE(76, 176, 206),
+        LIGHT_GRAY(131, 131, 126),
+        DEFAULT(113, 71, 45);
 
         private final Color color;
 
-        LeatherArmorColor(Color color) {
-            this.color = color;
+        LeatherArmorColor(int red, int green, int blue) {
+            this.color = new Color(red, green, blue);
         }
     }
 }
